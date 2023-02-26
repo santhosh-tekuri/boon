f03f3e6a62e67d0aae9dea9996f5976a3dbc1f9d
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2023-02-26 18:02:22 +0530

    suite: should run on any given testsuite

--- tests/suite.rs
+++ tests/suite.rs
@@ -4,8 +4,6 @@ use boon::{Compiler, Draft, Schemas, UrlLoader};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
-const SUITE_DIR: &str = "tests/JSON-Schema-Test-Suite";
-const TESTS_DIR: &str = "tests/JSON-Schema-Test-Suite/tests";
 static SKIP: [&str; 3] = [
     "zeroTerminatedFloats.json", // only draft4: this behavior is changed in later drafts
     "float-overflow.json",
@@ -28,35 +26,36 @@ struct Test {
 
 #[test]
 fn test_suite() -> Result<(), Box<dyn Error>> {
-    test_dir("draft4", Draft::V4)?;
-    test_dir("draft6", Draft::V6)?;
-    test_dir("draft7", Draft::V7)?;
-    test_dir("draft2019-09", Draft::V2019_09)?;
-    test_dir("draft2020-12", Draft::V2020_12)?;
+    let suite = "tests/JSON-Schema-Test-Suite";
+    test_dir(suite, "draft4", Draft::V4)?;
+    test_dir(suite, "draft6", Draft::V6)?;
+    test_dir(suite, "draft7", Draft::V7)?;
+    test_dir(suite, "draft2019-09", Draft::V2019_09)?;
+    test_dir(suite, "draft2020-12", Draft::V2020_12)?;
     Ok(())
 }
 
-fn test_dir(path: &str, draft: Draft) -> Result<(), Box<dyn Error>> {
-    let path = Path::new(TESTS_DIR).join(path);
-    for entry in path.read_dir()? {
+fn test_dir(suite: &str, path: &str, draft: Draft) -> Result<(), Box<dyn Error>> {
+    let prefix = Path::new(suite).join("tests");
+    for entry in prefix.join(path).read_dir()? {
         let entry = entry?;
         let file_type = entry.file_type()?;
         let entry_path = entry.path();
-        let entry_path = entry_path.strip_prefix(TESTS_DIR)?.to_str().unwrap();
+        let entry_path = entry_path.strip_prefix(&prefix)?.to_str().unwrap();
         if file_type.is_file() {
             if !SKIP.iter().any(|n| OsStr::new(n) == entry.file_name()) {
-                test_file(entry_path, draft)?;
+                test_file(suite, entry_path, draft)?;
             }
         } else if file_type.is_dir() {
-            test_dir(entry_path, draft)?;
+            test_dir(suite, entry_path, draft)?;
         }
     }
     Ok(())
 }
 
-fn test_file(path: &str, draft: Draft) -> Result<(), Box<dyn Error>> {
+fn test_file(suite: &str, path: &str, draft: Draft) -> Result<(), Box<dyn Error>> {
     println!("FILE: {path}");
-    let path = Path::new(TESTS_DIR).join(path);
+    let path = Path::new(suite).join("tests").join(path);
     let optional = path.components().any(|comp| comp.as_os_str() == "optional");
     let file = File::open(path)?;
 
@@ -71,8 +70,8 @@ fn test_file(path: &str, draft: Draft) -> Result<(), Box<dyn Error>> {
             compiler.enable_format_assertions();
             compiler.enable_content_assertions();
         }
-        compiler.register_url_loader("http", Box::new(RemotesLoader));
-        compiler.register_url_loader("https", Box::new(RemotesLoader));
+        compiler.register_url_loader("http", Box::new(RemotesLoader(suite.to_owned())));
+        compiler.register_url_loader("https", Box::new(RemotesLoader(suite.to_owned())));
         compiler.add_resource(url, group.schema)?;
         let sch_index = compiler.compile(url.into(), &mut schemas)?;
         for test in group.tests {
@@ -92,12 +91,12 @@ fn test_file(path: &str, draft: Draft) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-struct RemotesLoader;
+struct RemotesLoader(String);
 impl UrlLoader for RemotesLoader {
     fn load(&self, url: &str) -> Result<Value, Box<dyn std::error::Error>> {
         // remotes folder --
         if let Some(path) = url.strip_prefix("http://localhost:1234/") {
-            let path = Path::new(SUITE_DIR).join("remotes").join(path);
+            let path = Path::new(&self.0).join("remotes").join(path);
             let file = File::open(path)?;
             let json: Value = serde_json::from_reader(file)?;
             return Ok(json);
