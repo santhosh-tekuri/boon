c6df40f66a05e24722116340acb08cc57b4c5fe0
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2023-02-27 15:22:27 +0530

    tests/invalid-schema: test UnsupportedDraft & MetaSchemaCycle

--- tests/invalid-schemas.rs
+++ tests/invalid-schemas.rs
@@ -1,14 +1,15 @@
-use std::{error::Error, fs::File};
+use std::{collections::HashMap, error::Error, fs::File};
 
-use boon::{CompileError, Compiler, Schemas};
+use boon::{CompileError, Compiler, Schemas, UrlLoader};
 use serde::Deserialize;
 use serde_json::Value;
 
 #[derive(Debug, Deserialize)]
 struct Test {
     description: String,
+    remotes: Option<HashMap<String, Value>>,
     schema: Value,
-    errors: Vec<String>,
+    errors: Option<Vec<String>>,
 }
 
 #[test]
@@ -17,16 +18,19 @@ fn test_invalid_schemas() -> Result<(), Box<dyn Error>> {
     let tests: Vec<Test> = serde_json::from_reader(file)?;
     for test in tests {
         println!("{}", test.description);
-        match compile_schema(test.schema) {
+        match compile(&test) {
             Ok(_) => {
-                if !test.errors.is_empty() {
-                    Err("    expected compilation to fail")?;
+                if test.errors.is_some() {
+                    Err("want compilation to fail")?;
                 }
             }
             Err(e) => {
                 println!("   {e}");
                 let error = format!("{e:?}");
-                for want in &test.errors {
+                let Some(errors) = &test.errors else {
+                    return Err("want compilation to succeed")?;
+                };
+                for want in errors {
                     if !error.contains(want) {
                         println!("    got {error}");
                         println!("   want {want}");
@@ -39,11 +43,25 @@ fn test_invalid_schemas() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn compile_schema(v: Value) -> Result<(), CompileError> {
+fn compile(test: &Test) -> Result<(), CompileError> {
     let mut schemas = Schemas::new();
     let mut compiler = Compiler::new();
     let url = "http://fake.com/schema.json";
-    compiler.add_resource(url, v)?;
+    if let Some(remotes) = &test.remotes {
+        compiler.register_url_loader("http", Box::new(Remotes(remotes.clone())));
+    }
+    compiler.add_resource(url, test.schema.clone())?;
     compiler.compile(url.to_owned(), &mut schemas)?;
     Ok(())
+}
+
+struct Remotes(HashMap<String, Value>);
+
+impl UrlLoader for Remotes {
+    fn load(&self, url: &str) -> Result<Value, Box<dyn Error>> {
+        if let Some(v) = self.0.get(url) {
+            return Ok(v.clone());
+        }
+        Err("remote not found")?
+    }
 }
