b71eaa52fe79b9a114d61c2c05e88b62a587f748
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2023-03-13 17:52:19 +0530

    ecma: ensure no infinite recursion in convert

--- src/ecma.rs
+++ src/ecma.rs
@@ -1,6 +1,7 @@
 use std::borrow::Cow;
 
 use regex_syntax::ast::parse::Parser;
+use regex_syntax::ast::Error;
 use regex_syntax::ast::ErrorKind;
 
 // covert ecma regex to rust regex if possible
@@ -12,35 +13,41 @@ pub(crate) fn convert(pattern: &str) -> Cow<str> {
         let Err(e) = Parser::new().parse(pattern.as_ref()) else {
             break;
         };
-        if let ErrorKind::EscapeUnrecognized = e.kind() {
-            let (start, end) = (e.span().start.offset, e.span().end.offset);
-            let s = &e.pattern()[start..end];
-            match s {
-                r#"\/"# => {
-                    // handle escaping '/'
-                    pattern =
-                        Cow::Owned(format!("{}/{}", &e.pattern()[..start], &e.pattern()[end..],));
-                }
-                r#"\c"# => {
-                    // handle \c{control_letter}
-                    if let Some(control_letter) = e.pattern()[end..].chars().next() {
-                        if control_letter.is_ascii_alphabetic() {
-                            pattern = Cow::Owned(format!(
-                                "{}{}{}",
-                                &e.pattern()[..start],
-                                ((control_letter as u8) % 32) as char,
-                                &e.pattern()[end + 1..],
-                            ));
-                        }
-                    }
-                }
-                _ => break,
-            }
+        if let Some(s) = fix(e) {
+            pattern = Cow::Owned(s);
         } else {
             break;
         }
     }
     pattern
+}
+
+fn fix(e: Error) -> Option<String> {
+    if let ErrorKind::EscapeUnrecognized = e.kind() {
+        let (start, end) = (e.span().start.offset, e.span().end.offset);
+        let s = &e.pattern()[start..end];
+        match s {
+            r#"\/"# => {
+                // handle escaping '/'
+                return Some(format!("{}/{}", &e.pattern()[..start], &e.pattern()[end..],));
+            }
+            r#"\c"# => {
+                // handle \c{control_letter}
+                if let Some(control_letter) = e.pattern()[end..].chars().next() {
+                    if control_letter.is_ascii_alphabetic() {
+                        return Some(format!(
+                            "{}{}{}",
+                            &e.pattern()[..start],
+                            ((control_letter as u8) % 32) as char,
+                            &e.pattern()[end + 1..],
+                        ));
+                    }
+                }
+            }
+            _ => {}
+        }
+    }
+    None
 }
 
 #[cfg(test)]
@@ -51,5 +58,6 @@ mod tests {
     fn test_ecma_compat() {
         assert_eq!(convert(r#"ab\/cde\/fg"#), r#"ab/cde/fg"#); // '/' can be escaped
         assert_eq!(convert(r#"ab\cAcde\cBfg"#), "ab\u{1}cde\u{2}fg"); // \c{control_letter}
+        assert_eq!(convert(r#"\c\n"#), r#"\c\n"#); // \c{invalid_char}
     }
 }
