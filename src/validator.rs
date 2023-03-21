f6435626dbad2717e94eeff24b50d0a8d8c5acfb
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2023-03-21 21:09:16 +0530

    validate/perf: early return after type validations if possible

--- src/validator.rs
+++ src/validator.rs
@@ -160,10 +160,12 @@ impl<'v, 's, 'd> Validator<'v, 's, 'd> {
             _ => {}
         }
 
-        self.refs_validate(sloc.copy(), vloc.copy());
-        self.cond_validate(sloc.copy(), vloc.copy());
-        if s.draft_version >= 2019 && self.errors.is_empty() {
-            self.uneval_validate(sloc.copy(), vloc.copy());
+        if !self.bool_result || self.errors.is_empty() {
+            self.refs_validate(sloc.copy(), vloc.copy());
+            self.cond_validate(sloc.copy(), vloc.copy());
+            if s.draft_version >= 2019 && self.errors.is_empty() {
+                self.uneval_validate(sloc.copy(), vloc.copy());
+            }
         }
 
         match self.errors.len() {
