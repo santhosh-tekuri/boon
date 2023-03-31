b98bee7a2187819183a50d9ac829cc5d6a0206f2
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2023-04-01 02:28:12 +0530

    tests: add tests to gen

--- gen/src/lib.rs
+++ gen/src/lib.rs
@@ -1,3 +1,24 @@
 pub use boon::internal::*;
 pub use boon::*;
 pub use boon_procmacro::*;
+
+#[cfg(test)]
+mod tests {
+    use std::{env, error::Error, path::Path, process::Command};
+
+    #[test]
+    fn test_suite() -> Result<(), Box<dyn Error>> {
+        cargo_test()?;
+        Ok(())
+    }
+
+    fn cargo_test() -> Result<(), Box<dyn Error>> {
+        let mut cmd = Command::new(env::var("CARGO")?);
+        cmd.current_dir(Path::new(&env::var("CARGO_MANIFEST_DIR")?).join("tests/suite"));
+        cmd.args(["test", "-q", "--", "--nocapture"]);
+        if !cmd.spawn()?.wait()?.success() {
+            Err("cargo test failed")?
+        };
+        Ok(())
+    }
+}
---
+++ gen/tests/suite/src/lib.rs
@@ -0,0 +1,40 @@
+use boongen::compile;
+
+// todo: track files changes automatically
+const _: &[u8] = include_bytes!("../schema.json");
+
+#[compile(file = "schema.json")]
+struct Schema;
+
+#[cfg(test)]
+mod tests {
+    use std::{error::Error, fs::File};
+
+    use serde::Deserialize;
+    use serde_json::Value;
+
+    use super::Schema;
+
+    #[derive(Deserialize)]
+    struct Test {
+        description: String,
+        data: Value,
+        valid: bool,
+    }
+
+    #[test]
+    fn test() -> Result<(), Box<dyn Error>> {
+        let tests: Vec<Test> = serde_json::from_reader(File::open("tests.json")?)?;
+        let sch = Schema::new();
+        for test in tests {
+            println!("{}", test.description);
+            assert_eq!(
+                sch.is_valid0(&test.data),
+                test.valid,
+                "{}",
+                test.description
+            );
+        }
+        Ok(())
+    }
+}
