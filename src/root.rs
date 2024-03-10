8c70960f961a8a022b53bc20283b5867b46629e7
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2024-03-10 08:54:03 +0530

    error: fix typo

--- src/root.rs
+++ src/root.rs
@@ -49,7 +49,7 @@ impl Root {
         let (res_ptr, res) = {
             if url == self.url.as_str() {
                 let res = self.resources.get("").ok_or(CompileError::Bug(
-                    format!("no root resource found for{url}").into(),
+                    format!("no root resource found for {url}").into(),
                 ))?;
                 ("", res)
             } else {
