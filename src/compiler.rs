fb09df2db19be75c32c0970b4bdedf1655f5f943
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2024-07-30 20:37:19 +0530

    clippy: fix doc warnings

--- src/compiler.rs
+++ src/compiler.rs
@@ -107,9 +107,9 @@ impl Compiler {
 
     - for draft-07 and earlier: enabled
     - for draft/2019-09: disabled, unless
-    metaschema says `format` vocabulary is required
+      metaschema says `format` vocabulary is required
     - for draft/2020-12: disabled, unless
-    metaschema says `format-assertion` vocabulary is required
+      metaschema says `format-assertion` vocabulary is required
     */
     pub fn enable_format_assertions(&mut self) {
         self.assert_format = true;
